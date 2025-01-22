5d362c9718f3df1a72a70df817a7510a79b1da13
AP4ZS <szapa12345@gmail.com>
2025-01-22 13:41:42 +0100

    Added files program and created history file for calc

--- calc.py
+++ calc.py
@@ -1,86 +1,127 @@
 import subprocess
 
-j=1
+j = 1
 
-while j==1:
-    op=input('Podaj operację:')
-    if op=='+':
-        a=[]
-        l=0
-        k=input('Liczba cyfr w dodawaniu: ')
+# Funkcja do zapisywania historii działań do pliku
+def zapisz_do_histori(opis):
+    with open("historia.txt", "a") as f:
+        f.write(opis + "\n")
+
+while j == 1:
+    op = input('Podaj operację: ')
+
+    if op == '+':
+        a = []
+        l = 0
+        k = input('Liczba cyfr w dodawaniu: ')
+        if not k.isdigit():
+            print("Podaj liczbę całkowitą")
+            continue
         for l in range(int(k)):
-            a.append(int(input(str(l+1)+' liczba: ')))
-            l=l+1
-        wynik=sum(a)
+            a.append(int(input(str(l + 1) + ' liczba: ')))
+            l = l + 1
+        wynik = sum(a)
         print(wynik)
 
-    elif op=='-':
+        # Zapisz do historii
+        zapisz_do_histori(f'Dodawanie: {" + ".join(map(str, a))} = {wynik}')
+
+    elif op == '-':
         a = []
         l = 0
         k = input('Liczba cyfr w odejmowaniu: ')
-        for l in range (int(k)):
-            a.append(int(input(str(l+1) + ' liczba: ')))
+        if not k.isdigit():
+            print("Podaj liczbę całkowitą")
+            continue
+        for l in range(int(k)):
+            a.append(int(input(str(l + 1) + ' liczba: ')))
             l = l + 1
         wynik = a[0]
-        b=1
-        k=len(a)
-        for b in range (1, k):
+        b = 1
+        k = len(a)
+        for b in range(1, k):
             wynik -= a[b]
-            b=b+1
+            b = b + 1
         print(wynik)
 
+        # Zapisz do historii
+        zapisz_do_histori(f'Odejmowanie: {" - ".join(map(str, a))} = {wynik}')
 
-
-
-    elif op=='*':
+    elif op == '*':
         a = []
         l = 0
         k = input('Liczba cyfr w mnożeniu: ')
+        if not k.isdigit():
+            print("Podaj liczbę całkowitą")
+            continue
         for l in range(int(k)):
-            a.append(int(input(str(l+1) + ' liczba: ')))
+            a.append(int(input(str(l + 1) + ' liczba: ')))
         wynik = a[0]
         b = 1
-        k=len(a)
-        for b in range (1, k):
+        k = len(a)
+        for b in range(1, k):
             wynik = wynik * a[b]
-        print (wynik)
-    elif op=='/':
+        print(wynik)
+
+        # Zapisz do historii
+        zapisz_do_histori(f'Mnożenie: {" * ".join(map(str, a))} = {wynik}')
+
+    elif op == '/':
         a = []
         l = 0
         k = input('Liczba cyfr w dzieleniu: ')
+        if not k.isdigit():
+            print("Podaj liczbę całkowitą")
+            continue
         for l in range(int(k)):
-            a.append(int(input(str(l+1) + ' liczba: ')))
+            a.append(int(input(str(l + 1) + ' liczba: ')))
         wynik = a[0]
         b = 1
-        k=len(a)
-        for b in range (1, k):
+        k = len(a)
+        for b in range(1, k):
             wynik = wynik / a[b]
-        print (wynik)
+        print(wynik)
 
-    elif op=='!':
+        # Zapisz do historii
+        zapisz_do_histori(f'Dzielenie: {" / ".join(map(str, a))} = {wynik}')
+
+    elif op == '!':
         l = int(input('Podaj silnię: '))
-        a = list(range(1, l+1))
+        if not str(l).isdigit():
+            print("Podaj liczbę całkowitą")
+            continue
+        a = list(range(1, l + 1))
         k = len(a)
         b = 1
         wynik = a[0]
-        for b in range (1, k):
+        for b in range(1, k):
             wynik = wynik * a[b]
-        print (wynik)
+        print(wynik)
 
+        # Zapisz do historii
+        zapisz_do_histori(f'Silnia: {l}! = {wynik}')
 
-
-    elif op=='^':
+    elif op == '^':
         l = 1
         k = int(input('Potęgowana liczba: '))
+        if not str(k).isdigit():
+            print("Podaj liczbę całkowitą")
+            continue
         m = int(input('Do potęgi: '))
+        if not str(m).isdigit():
+            print("Podaj liczbę całkowitą")
+            continue
         m -= 1
         wynik = k
         for l in range(int(m)):
             wynik = wynik * k
-        print (wynik)
+        print(wynik)
 
-    elif op=='q':
-        j=0
+        # Zapisz do historii
+        zapisz_do_histori(f'Potęgowanie: {k}^{m + 1} = {wynik}')
+
+    elif op == 'q':
+        j = 0
 
     else:
         print('Dostępne operacje: \n'
--- files.py
+++ files.py
@@ -1,7 +1,47 @@
 import os
+import subprocess
 j = 1
 
-if j == 1:
-    print()
-    i = input('')
-    break
+os.chdir("C:\\")
+while j == 1:
+    i = input('Command_Files: ')
+
+    if i == 'q':
+        j = 0
+
+    elif i == 'ls':
+        print(os.getcwd())
+        print(os.listdir())
+
+    elif i.startswith('cd'):
+        if i == 'cd':
+            print('Brak argumentu')
+        else:
+            os.chdir(i[3:])
+            print(os.getcwd())
+
+    elif i.startswith('head'):
+        if i == 'head':
+            print('Brak argumentu')
+        else:
+            try:
+                with open(i[5:],'r') as file:
+                    k = file.read(512)
+                    print(k)
+            except FileNotFoundError:
+                print('Nie odnaleziono pliku')
+            except PermissionError:
+                print('Brak dostępu')
+
+    elif i == 'pwd':
+        print(os.getcwd())
+
+    else:
+        print('Komenda nie istnieje. Dostępne komendy: \n'
+              'q - wyjście \n'
+              'ls - wyświetl listę plików w bieżącym katalogu \n'
+              'cd $1 - przejdź do katalogu \n'
+              'pwd - wyświetla bieżącą ścieżkę \n'
+              'head $1 - wyświetla pierwsze 512 bajtów pliku')
+
+subprocess.run(["python", "Main.py"])
